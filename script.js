b32a1a6d445650e616fbdba1d4248d7844d367dd
Devman869 <devtamang870@gmail.com>
2025-05-07 13:34:27 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Example 1: Changing text
+function changeText() {
+    document.getElementById("demo").innerHTML = "You clicked the button!";
+  }
+  
+  // Example 2: Changing style
+  function changeStyle() {
+    document.getElementById("styleDemo").style.color = "red";
+    document.getElementById("styleDemo").style.fontSize = "20px";
+  }
+  
+  // Example 3: Handling mouseover event
+  function showMessage() {
+    document.getElementById("message").style.display = "block";
+  }
+  
